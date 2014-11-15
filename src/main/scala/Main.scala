dc64c4dc135235a3b36701f5fcc09771a5374a8f
Piyush Purang <ppurang@gmail.com>
2014-11-15 12:23:03 +0100

    outgoing incoming tx log types

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -31,34 +31,43 @@ object Main {
   var processedOutTransactions = Set[String]()
   var processedInTransactions = Set[String]()
 
+  case class IncomingTxLog(txs: List[Transaction])
+  case class OutgoingTxLog(txs: List[Transaction])
+
+
   def mainLoop(): Unit = {
     val txLog = getTransactionList()
-    val (in, out) = splitInOut(txLog)
+    val (out, in) = splitOutIn(txLog)
     markCompletedOutTransactions(out)
     val unprocessedOut = findUnprocessedInTransactions(in)
     createOutTransactions(unprocessedOut)
     runOutTransactions()
   }
 
-  def splitInOut(txLog: List[Transaction]): (List[Transaction], List[Transaction]) = {
-    ???
-  }
-
   def getTransactionList(): List[Transaction] = {
     API.account_tx(account)
   }
-  def markCompletedOutTransactions(transactions: List[Transaction]): Unit = {
+
+  def splitOutIn(txLog: List[Transaction]): (OutgoingTxLog, IncomingTxLog) = {
+    val (outgoing, incoming) = txLog.filter(_.isPayment).partition(_.account == account)
+    (OutgoingTxLog(outgoing), IncomingTxLog(incoming))
+  }
+
+  def markCompletedOutTransactions(transactions: OutgoingTxLog): Unit = {
     // move transactions from inProcessOutTransactions to processedOutTransactions
     ???
   }
-  def findUnprocessedInTransactions(transactions: List[Transaction]): List[Transaction] = {
+
+  def findUnprocessedInTransactions(transactions: IncomingTxLog): List[Transaction] = {
     // finds transactions not in processedInTransactions
     ???
   }
+
   def createOutTransactions(transactions: List[Transaction]): Unit = {
     // adds object to inProcessOutTransactions and removes to processedInTransactions
     ???
   }
+
   def runOutTransactions(): Unit = {
     /// runs all transactions in inProcessOutTransactions again
     ???
